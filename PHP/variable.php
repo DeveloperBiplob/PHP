f129b51879ff5becf75bbbe9529d3d31dee560fb
BIPLOB <developerbiplob.bd@gmail.com>
2022-05-20 10:25:54 +0600

    PHP | Variable

---
+++ PHP/variable.php
@@ -0,0 +1,12 @@
+<?php
+
+// PHP Variable-----------------//
+// PHP case-sensitive. like [$a, $A, $b, $B] ai gulu shob gulu alada variable.
+
+$name = "biplob";
+$number = 1000;
+
+echo $name;
+echo $number;
+
+?>
