6c59f86e9e08123b04f4f9aec46745fe64ed167b
BIPLOB <developerbiplob.bd@gmail.com>
2022-05-20 10:44:05 +0600

    PHP | Data Types

---
+++ PHP/data-type.php
@@ -0,0 +1,20 @@
+<?php
+
+// HPP Data Types-------//
+
+// string
+$name = 'Biplob';
+
+// Integer
+$number = 50;
+
+//Float
+$num = 50.50;
+
+// Array
+$students = ['biplob', 'bipu', 'bipul'];
+$users = array('korim', 'manik', 'sobuj');
+
+
+
+?>
