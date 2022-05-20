6415d5276342a17740b10bba634879691b78a3dc
BIPLOB <developerbiplob.bd@gmail.com>
2022-05-20 12:06:23 +0600

    PHP | Associative Array

---
+++ PHP/associativeArray.php
@@ -0,0 +1,20 @@
+<?php
+
+// PHP associative array
+
+$studentDetails = [
+    'name' => 'Biplob',
+    'age' => 20,
+    'class' => 10,
+    'address' => 'North Badda'
+];
+
+echo 'Student Name is: ' . $studentDetails["name"];
+echo "<br/>";
+echo 'Student Age is: ' . $studentDetails["age"];
+echo "<br/>";
+echo 'Student Class is: ' . $studentDetails["class"];
+echo "<br/>";
+echo 'Student Address is: ' . $studentDetails["address"];
+
+?>
