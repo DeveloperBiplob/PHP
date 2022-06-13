4273da800de2c08dc9496afd14b6890aefbc0e7a
BIPLOB <developerbiplob.bd@gmail.com>
2022-06-13 10:15:35 +0600

    PHP | Include_once, Require, and Require_once

---
+++ PHP/includes/includes_once.php
@@ -0,0 +1,28 @@
+<?php include_once './header.php'; ?>
+
+<div class="container">
+<h1>FIle included successfully</h1>
+</div>
+
+<?php include_once './footer.php'; ?>
+<?php include_once './footer.php'; ?>
+<?php include_once './footer.php'; ?>
+
+<!--File joto bar e include kora hik na keno,
+ include_once use korle seta akbar e includes hobe. -->
+
+
+
+
+ 
+ <!-- Require and Requied_once same -->
+
+
+<?php require './header.php'; ?>
+
+<div class="container">
+<h1>FIle included successfully Using Requied and Require_once</h1>
+</div>
+
+<?php require_once './footer.php'; ?>
+<?php require_once './footer.php'; ?>
